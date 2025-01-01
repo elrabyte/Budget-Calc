f025e8a8a8348158363a673262a5ac5252466421
elrabyte <83375061+elrabyte@users.noreply.github.com>
2025-01-01 14:44:35 +0100

    init

---
+++ budget-calc/app/types/entry.tsx
@@ -0,0 +1,9 @@
+import { Frequency } from "./frequency";
+
+export type entry = {
+  category: string;
+  description: string;
+  frequency: Frequency;
+  payment_account: string;
+  price: number;
+};
